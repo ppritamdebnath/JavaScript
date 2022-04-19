e12bda2479bdc224326d4ab37feb5578cb6a2b51
Pritam Debnath <33841691+ppritamdebnath@users.noreply.github.com>
2022-04-19 14:32:21 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -2,3 +2,31 @@
 let message;
 message = 'Hello!';
 alert(message);
+
+let message1 = 'Hello1111!'; // define the variable and assign the value
+alert(message1);
+
+let message2;
+message2 = 'Hello2!';
+message2 = 'World2!'; // value changed
+alert(message2);
+
+let hello = 'Hello world!';
+let message3;
+message3 = hello;
+alert(hello); // Hello world!
+alert(message); // Hello world!
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+alert($ + _); // 3
+
+num = 5; // the variable "num" is created if it didn't exist
+alert(num); // 5
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_ORANGE;
+alert(color);*/
